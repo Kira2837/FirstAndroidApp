95fd60f9e88a57b7045db8dd4efbb60ed50682de
AngelCake <ignat.skochilov@gmail.com>
2024-03-17 16:42:38 +0300

    ver2 (with image picker)

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myfirstapp
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -20,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val changeActivity: Button = findViewById(R.id.changeActivity)
+        val imageView: ImageView = findViewById(R.id.imageView)
+        val getImage: Button = findViewById(R.id.getImageButton)
         val dataString: EditText = findViewById(R.id.dataString)
 
         changeActivity.setOnClickListener{
@@ -30,7 +36,18 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+
+        val imageR = registerForActivityResult(ActivityResultContracts.GetContent(),
+        ActivityResultCallback{
+            imageView.setImageURI(it)
+        })
+
+        getImage.setOnClickListener{
+
+            imageR.launch("image/*")
+        }
     }
+
 }
 
 
